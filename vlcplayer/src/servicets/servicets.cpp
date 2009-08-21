555851c3ed45baa472fd5585d95ce1d5cb5e96de
Andreas Monzner <andreas.monzner@multimedia-labs.de>
2009-08-21 18:21:03 +0000

    backport to old enigma2 decoder api

--- vlcplayer/src/servicets/servicets.cpp
+++ vlcplayer/src/servicets/servicets.cpp
@@ -274,8 +274,9 @@ RESULT eServiceTS::start()
 	m_decoder->setAudioPID(m_apid, eDVBAudio::aMPEG);
 	m_streamthread = new eStreamThread();
 	CONNECT(m_streamthread->m_event, eServiceTS::recv_event);
-	m_decoder->pause();
-	if (unpause() != 0) 
+	m_decoder->freeze(0);
+	m_decoder->preroll();
+	if (unpause() != 0)
 		return -1;
 	m_event(this, evStart);
 	return 0;
@@ -305,7 +306,7 @@ void eServiceTS::recv_event(int evt)
 		break;
 	case eStreamThread::evtReadError:
 	case eStreamThread::evtWriteError:
-		m_decoder->pause();
+		m_decoder->freeze(0);
 		m_event((iPlayableService*)this, evEOF);
 		break;
 	case eStreamThread::evtSOS:
@@ -337,7 +338,7 @@ RESULT eServiceTS::pause(ePtr<iPauseableService> &ptr)
 RESULT eServiceTS::pause()
 {
 	m_streamthread->stop();
-	m_decoder->pause();
+	m_decoder->freeze(0);
 	return 0;
 }
 
@@ -360,7 +361,7 @@ RESULT eServiceTS::unpause()
 
 		m_decodedemux->flush();
 		m_streamthread->start(srcfd, m_destfd);
-		m_decoder->play();
+		m_decoder->unfreeze();
 	}
 	else
 		eDebug("unpause but thread already running!");
@@ -441,7 +442,7 @@ RESULT eServiceTS::selectTrack(unsigned int i) {
 		m_apid = m_audioInfo->audioStreams[i].pid;
 		eDebug("[servicets] audio track %d PID 0x%02x type %d\n", i, m_apid, m_audioInfo->audioStreams[i].type);
 		m_decoder->setAudioPID(m_apid, m_audioInfo->audioStreams[i].type);
-		m_decoder->set();
+		m_decoder->preroll();
 		return 0;
 	} else {
 		return -1;
