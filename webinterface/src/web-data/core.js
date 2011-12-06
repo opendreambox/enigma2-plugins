3cd4ef6bf551451a22645d3eddea91ec422eb238
U-sheldon\sre <sre@sheldon.(none)>
2011-12-07 00:17:02 +0100

    more fixes (epg search, missing stuff)

--- webinterface/src/web-data/core.js
+++ webinterface/src/web-data/core.js
@@ -1199,7 +1199,7 @@ var E2WebCore = Class.create({
 			'submit',
 			function(event, element){
 				this.epg.search($F('epgSearch'));
-				return false;
+				event.stop();
 			}.bind(this)
 		);		
 		$('epgSearch').on(
@@ -1336,6 +1336,14 @@ var E2WebCore = Class.create({
 			function(event, element){
 				this.saveSettings();
 			}.bind(this)
+		);
+		//Bouquets
+		content.on(
+			'click',
+			'a.bListSLink',
+			function(event, element){
+				setContentHd(element.readAttribute("data-servicename"));
+			}
 		);
 		//Servicelist
 		content.on(
