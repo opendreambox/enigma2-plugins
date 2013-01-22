2c08acfca2f8014d2c467d12c89539e6a131348f
Moritz Venn <moritz.venn@freaque.net>
2013-01-22 16:05:59 +0100

    AutoTimerLogger.py: simplify by using the print function

--- autotimer/src/AutoTimerLogger.py
+++ autotimer/src/AutoTimerLogger.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from . import _, ATLOG_LIST
 from Components.config import config
 
@@ -7,30 +9,20 @@ ATLOG_WARN = "3"
 ATLOG_ERROR = "4"
 
 ATLOG_MESSAGES = {"1": "DEBUG",
-                  "2": "INFO ",
-                  "3": "WARN ",
-                  "4": "ERROR"}
+				"2": "INFO ",
+				"3": "WARN ",
+				"4": "ERROR"}
+
+logLevelEl = config.plugins.autotimer.loglevel
 
 def atLog(logLevel, *args):
-    cfgLogLevel = config.plugins.autotimer.loglevel.value
-    if logLevel >= cfgLogLevel:
-        strargs = ""
-        logLevelText = ATLOG_MESSAGES[logLevel] if logLevel in ATLOG_MESSAGES.keys() else "?????"
-        for arg in args:
-            if strargs: strargs += " "
-            strargs += str(arg)
-        print "[AutoTimer " + logLevelText + "] " + strargs
-        
-        if config.plugins.autotimer.logwrite.value:
-            strargs += "\n"
-            
-            # Append to file
-            f = None
-            try:
-                f = open(config.plugins.autotimer.logfile.value, 'a')
-                f.write("[" + logLevelText + "] " + strargs)
-            except Exception, e:
-                print "[AutoTimer] atLog exception " + str(e)
-            finally:
-                if f:
-                    f.close()
+	if logLevel >= logLevelEl.value:
+		logLevelText = ATLOG_MESSAGES[logLevel] if logLevel in ATLOG_MESSAGES.keys() else "?????"
+		print("[AutoTimer", logLevelText + "]", *args)
+
+		if config.plugins.autotimer.logwrite.value:
+			try:
+				with open(config.plugins.autotimer.logfile.value, 'a') as f:
+					print("[" + logLevelText + "]", *args, file=f)
+			except Exception, e:
+				print("[AutoTimer] atLog exception", e)
