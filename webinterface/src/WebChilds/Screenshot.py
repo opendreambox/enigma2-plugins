e65dfb713dc0d1902046cc4f55a455729cf7d984
Stephan Reichholf <reichi@opendreambox.org>
2014-08-10 12:06:11 +0200

    WebInterface: fix screenshot handling

--- webinterface/src/WebChilds/Screenshot.py
+++ webinterface/src/WebChilds/Screenshot.py
@@ -4,7 +4,9 @@ from twisted.web import resource, http, http_headers, server
 from Tools.Log import Log
 
 class ScreenshotResource(resource.Resource):
-	SPECIAL_ARGS = ('format', 'filename', 'save')
+	def __init__(self):
+		resource.Resource.__init__(self)
+		self._buffer = None
 
 	def render(self, request):
 		args = []
@@ -13,58 +15,55 @@ class ScreenshotResource(resource.Resource):
 		# some presets
 		filename = 'screenshot'
 		imageformat = ePixmap.FMT_JPEG
-		extension = 'jpg'
+		format = 'jpg'
 		jpgquali = '80'
 		osd = False
 		video = False
-		save = False
 
 		for key, value in request.args.items():
-			if key in ScreenshotResource.SPECIAL_ARGS:
-				if key == 'extension':
-					extension = value[0]
-
-					if extension == 'jpg':
-						#-j (quality) produce jpg files instead of bmp
-						#Quality Setting
-						if 'jpgquali' in request.args:
-							jpgquali = request.args["jpgquali"][0]
-							del request.args['jpgquali']
-					if extension == 'png':
-						imageformat = ePixmap.FMT_PNG
-					if extension == 'gif':
-						imageformat = ePixmap.FMT_GIF
-				elif key == 'filename':
-					filename = value[0]
-
-				elif key == 'save':
-					save = True
+			if key == 'format':
+				format = value[0]
+				if format == 'jpg':
+					#-j (quality) produce jpg files instead of bmp
+					#Quality Setting
+					if 'jpgquali' in request.args:
+						jpgquali = request.args["jpgquali"][0]
+						del request.args['jpgquali']
+				if format == 'png':
+					imageformat = ePixmap.FMT_PNG
+				if format == 'gif':
+					imageformat = ePixmap.FMT_GIF
+			elif key == 'filename':
+				filename = value[0]
 			else:
-				if key == "o":
+				if key == "osd":
 					osd = True
-				if key == "v":
+				if key == "video":
 					video = True
 		if not osd and not video:
 			osd = video = True
 		
-		filename = "%s.%s" %(filename, extension)
-		
+		filename = "%s.%s" %(filename, format)
+		Log.i("osd=%s, video=%s, filename=%s" %(osd, video, filename))
 		request.setHeader('Content-Disposition', 'inline; filename=%s;' %filename)
-		mimetype = {'jpg' : 'jpeg'}.get(extension, extension)
+		mimetype = {'jpg' : 'jpeg'}.get(format, format)
 		request.setHeader('Content-Type','image/%s' %mimetype)
 		pixmap = ePixmap(None)
 		size = getDesktop(0).size()
 		if osd:
 			if video:
-				pixmap.setPixmapFromScreen(size)
+				if not pixmap.setPixmapFromScreen(size):
+					Log.w("Failed setting pixmap from Screen!")
 			else:
-				pixmap.setPixmapFromUI(size)
+				if not pixmap.setPixmapFromUI(size):
+					Log.w("Failed setting pixmap from UI!")
 		elif video:
-			pixmap.setPixmapFromVideo(0, size)
+			if not pixmap.setPixmapFromVideo(0, size):
+				Log.w("Failed setting pixmap from Video!")
 
-#		if save:
-#			pixmap.save(imageformat, "/tmp/%s" %filename)
-		
 		buffer = pixmap.save(imageformat)
-		buffer.data()
-		return bytes
+		if buffer:
+			bytes = buffer.data()
+			request.write(str(bytes))
+			request.finish()
+		return server.NOT_DONE_YET
