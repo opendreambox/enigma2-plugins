4954401e30142b23d17646bf459ec065d83e7d93
joe128 <johnny.u@gmx.at>
2014-08-22 07:15:57 +0200

    2.1.3: .) fix: switch to previous service in idle-mode .) some fixes in config-order .) Bugfix in "skipping service if protected" .) de- and nl-locale updated, en-locale added(to translate code-strings)

--- epgrefresh/src/__init__.py
+++ epgrefresh/src/__init__.py
@@ -4,15 +4,29 @@ from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
 from os import environ as os_environ
 import gettext
 
+PluginLanguageDomain = "EPGRefresh"
+PluginLanguagePath = "Extensions/EPGRefresh/locale"
+# Fallback to EN for Code-Strings
+DefaultPluginLang = "EN"
+
 def localeInit():
 	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
 	os_environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
-	gettext.bindtextdomain("EPGRefresh", resolveFilename(SCOPE_PLUGINS, "Extensions/EPGRefresh/locale"))
+	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 
 def _(txt):
-	t = gettext.dgettext("EPGRefresh", txt)
+	t = gettext.dgettext(PluginLanguageDomain, txt)
 	if t == txt:
-		t = gettext.gettext(txt)
+		t = getDefaultTxt(txt)
+		if t == txt:
+			t = gettext.gettext(txt)
+	return t
+
+def getDefaultTxt(txt):
+	lang = language.getLanguage()[:2]
+	os.environ["LANGUAGE"] = DefaultPluginLang
+	t = gettext.dgettext(PluginLanguageDomain, txt)
+	os.environ["LANGUAGE"] = lang
 	return t
 
 localeInit()
