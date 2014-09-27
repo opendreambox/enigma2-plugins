da5c129bc71058beb0fd01dbb18ab4f6cf7a97bc
Stephan Reichholf <reichi@opendreambox.org>
2014-09-27 16:33:05 +0200

    eventlist: fix for new epgcache, simplify code a bit

--- eventlist/src/EventList.py
+++ eventlist/src/EventList.py
@@ -37,14 +37,14 @@ class EventList(Converter, object):
 		if (len(type)):
 			args = type.split(',')
 			i = 0
-			while i <= len(args)-1:
+			while i <= len(args) - 1:
 				type_c, value = args[i].split('=')
 				if type_c == "eventcount":
-					self.eventcount = int(value)			
+					self.eventcount = int(value)
 				elif type_c == "primetime":
 					if value == "yes":
 						self.primetime = 1
-				i +=1  			
+				i += 1
 	@cached
 	def getContent(self):
 		contentList = []
@@ -52,32 +52,33 @@ class EventList(Converter, object):
 		info = ref and self.source.info
 		if info is None:
 			return []
-		curEvent = self.source.getCurrentEvent()
-		if curEvent:
-			if not self.epgcache.startTimeQuery(eServiceReference(ref.toString()), curEvent.getBeginTime() + curEvent.getDuration()):
-				i = 1
-				while i <= (self.eventcount):
-					event = self.epgcache.getNextTimeEntry()
-					if event is not None:
-						contentList.append(self.getEventTuple(event),)
-					i +=1
-				if self.primetime == 1:
-					now = localtime(time())
-					dt = datetime(now.tm_year, now.tm_mon, now.tm_mday, 20, 15)
-					if time() > mktime(dt.timetuple()):
-						dt += timedelta(days=1) # skip to next day...
-					primeTime = int(mktime(dt.timetuple()))
-					if not self.epgcache.startTimeQuery(eServiceReference(ref.toString()), primeTime):
-						event = self.epgcache.getNextTimeEntry()
-						if event and (event.getBeginTime() <= int(mktime(dt.timetuple()))):
-							contentList.append(self.getEventTuple(event),)
+		event = self.source.getCurrentEvent()
+		if not event:
+			return contentList
+
+		i = 1
+		while i <= (self.eventcount) and event:
+			event = self.epgcache.lookupEventTime(eServiceReference(ref.toString()), event.getBeginTime() + event.getDuration())
+			if event:
+				contentList.append(self.getEventTuple(event),)
+			i += 1
+		if self.primetime == 1:
+			now = localtime(time())
+			dt = datetime(now.tm_year, now.tm_mon, now.tm_mday, 20, 15)
+			if time() > mktime(dt.timetuple()):
+				dt += timedelta(days=1) # skip to next day...
+			primeTime = int(mktime(dt.timetuple()))
+			event = self.epgcache.lookupEventTime(eServiceReference(ref.toString()), primeTime)
+			if event and (event.getBeginTime() <= int(mktime(dt.timetuple()))):
+				contentList.append(self.getEventTuple(event),)
+
 		return contentList
 
-	def getEventTuple(self,event):
+	def getEventTuple(self, event):
 		time = "%s - %s" % (strftime("%H:%M", localtime(event.getBeginTime())), strftime("%H:%M", localtime(event.getBeginTime() + event.getDuration())))
 		title = event.getEventName()
 		duration = "%d min" % (event.getDuration() / 60)
-		return (time,title,duration)
+		return (time, title, duration)
 
 	def changed(self, what):
 		if what[0] != self.CHANGED_SPECIFIC:
