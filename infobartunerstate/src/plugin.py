bdd9e101f3417903fabab5760ad01e4d86844334
Frank Glaser <glaserfrank@gmail.com>
2014-10-31 14:31:14 +0100

    InfoBarTunerState 1.1.6: Added SecondInfobar Handling

--- infobartunerstate/src/plugin.py
+++ infobartunerstate/src/plugin.py
@@ -37,7 +37,7 @@ from InfoBarTunerState import InfoBarTunerState, TunerStateInfo
 NAME = _("InfoBarTunerState")
 IBTSSHOW = _("Show InfoBarTunerState")
 IBTSSETUP = _("InfoBarTunerState Setup")
-VERSION = "1.1.5"
+VERSION = "1.1.6"
 SUPPORT = "http://bit.ly/ibtsihad"
 DONATE = "http://bit.ly/ibtspaypal"
 ABOUT = "\n  " + NAME + " " + VERSION + "\n\n" \
