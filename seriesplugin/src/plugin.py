2f40e8995381a628b149cf6c39a8331185359c4f
Frank Glaser <glaserfrank@gmail.com>
2014-11-18 20:29:45 +0100

    SeriesPlugin 1.4.1: Fixed glob escape is only available in Python 3.x

--- seriesplugin/src/plugin.py
+++ seriesplugin/src/plugin.py
@@ -28,7 +28,7 @@ from Logger import splog
 #######################################################
 # Constants
 NAME = "SeriesPlugin"
-VERSION = "1.4"
+VERSION = "1.4.1"
 DESCRIPTION = _("SeriesPlugin")
 SHOWINFO = _("Show series info (SP)")
 RENAMESERIES = _("Rename serie(s) (SP)")
