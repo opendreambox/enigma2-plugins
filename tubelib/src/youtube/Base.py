7808817030be6f711ab941168f8ff9bd447ca694
Stephan Reichholf <reichi@opendreambox.org>
2015-12-02 11:17:30 +0100

    tubelib: fix token refresh not working by doing some python magic. Fix Makefile.am

--- tubelib/src/youtube/Base.py
+++ tubelib/src/youtube/Base.py
@@ -1,11 +1,21 @@
+from apiclient.discovery import build
+from oauth2client.file import Storage
+from oauth2client.client import OAuth2Credentials
+import httplib2
+
+from YoutubeAuth import YoutubeAuth
+
 DEVELOPER_KEY = "AIzaSyDUp-wKizINw0m6n_7_DInK6A4qKnMaKFU"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-from apiclient.discovery import build
-from oauth2client.file import Storage
-
-import httplib2
+def scopedCredentials(scope):
+	original_apply = OAuth2Credentials.apply
+	def apply(self, headers):
+		if not scope in headers:
+			headers["scope"] = scope
+		original_apply(self, headers)
+	OAuth2Credentials.apply = apply
 
 def loadCredentials(credentials_file):
 	credentials = None
@@ -21,8 +31,9 @@ def saveCredentials(credentials_file, credentials):
 	except:
 		return False
 
-def buildYoutube(credentials=None):
+def buildYoutube(credentials=None,scope=YoutubeAuth.AUTH_SCOPE_YT_RO):
 	if credentials:
+		scopedCredentials(scope)
 		return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY, http=credentials.authorize(httplib2.Http()))
 	else:
 		return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
