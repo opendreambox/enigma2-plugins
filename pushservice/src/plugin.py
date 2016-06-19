ef55a1aa271ff4c37522f0886e360876c82cb3ce
Frank Glaser <glaserfrank@gmail.com>
2016-06-19 14:28:41 +0200

    PushService 0.4.4: New Controller: BrokenRecords: Find records which are from yesterday and smaller than 10MB

--- pushservice/src/plugin.py
+++ pushservice/src/plugin.py
@@ -35,7 +35,7 @@ from PushService import PushService
 
 # Constants
 NAME = "PushService"
-VERSION = "0.4.3"
+VERSION = "0.4.4"
 SUPPORT = "http://bit.ly/psihad"
 DONATE = "http://bit.ly/pspaypal"
 ABOUT = "\n  " + NAME + " " + VERSION + "\n\n" \
