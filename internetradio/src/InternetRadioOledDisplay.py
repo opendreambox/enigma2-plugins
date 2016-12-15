94e10db1dbd9353e3aaaa5686360395d6376ffae
Dr.Best <dr.best@dreambox-tools.info>
2016-12-15 22:42:51 +0100

    [InternetRadio] support for dm900 display

--- internetradio/src/InternetRadioOledDisplay.py
+++ internetradio/src/InternetRadioOledDisplay.py
@@ -23,24 +23,12 @@ from Screens.Screen import Screen
 from Components.ProgressBar import ProgressBar
 from Components.Pixmap import Pixmap
 from Components.Label import Label
+from Components.Sources.StaticText import StaticText
 from InternetRadioVisualization import InternetRadioVisualization
 
 class InternetRadioOledDisplay(Screen, InternetRadioVisualization):
 
-		# helper for skinning
-#		skincontent = ""
-#		skincontent2 = ""
-#		count = 16
-#		x = 0
-#		posx = 1
-#		skinid = 0
-#		while True:
-#			skincontent += "<widget name=\"progress_%d\" zPosition=\"3\" position=\"%d,5\" size=\"5,50\" transparent=\"1\" orientation=\"orBottomToTop\" pixmap=\"/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_oled-fs8.png\" />\n" % (x,posx)
-#			skincontent2 += "<widget name=\"top_%d\" position=\"%d,5\" zPosition=\"6\" size=\"5,2\" transparent=\"1\" pixmap=\"/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_oled_top-fs8.png\" />\n" % (x,posx)
-#			posx += 6
-#			x += 1
-#			if x == count:
-#				break
+
 
 	skin = ("""
 		<screen name="InternetRadioOledDisplay" position="0,0" size="96,64" id="2">
@@ -79,6 +67,42 @@ class InternetRadioOledDisplay(Screen, InternetRadioVisualization):
 			<widget name="top_14" position="85,5" zPosition="6" size="5,2" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_oled_top-fs8.png" />
 			<widget name="top_15" position="91,5" zPosition="6" size="5,2" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_oled_top-fs8.png" />
 		</screen>""","""
+		<screen name="InternetRadioOledDisplay" backgroundColor="#00000000" position="0,0" size="400,240" id="3">
+			<widget name="text1" zPosition="1" position="0,5" size="400,40" font="Regular;28" halign="center" valign="center" foregroundColor="#fcc000" backgroundColor="#00000000"/>
+			<widget name="text2" zPosition="1" position="0,45" size="400,195" font="Regular;32" halign="center" valign="center" foregroundColor="#fcc000" backgroundColor="#00000000"/>
+			<widget name="progress_0" zPosition="3" position="30,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_1" zPosition="3" position="51,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_2" zPosition="3" position="72,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_3" zPosition="3" position="93,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_4" zPosition="3" position="114,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_5" zPosition="3" position="135,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_6" zPosition="3" position="156,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_7" zPosition="3" position="177,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_8" zPosition="3" position="198,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_9" zPosition="3" position="219,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_10" zPosition="3" position="240,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_11" zPosition="3" position="261,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_12" zPosition="3" position="282,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_13" zPosition="3" position="303,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_14" zPosition="3" position="324,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="progress_15" zPosition="3" position="345,30" size="20,200" transparent="1" orientation="orBottomToTop" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png" />
+			<widget name="top_0" position="30,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_1" position="51,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_2" position="72,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_3" position="93,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_4" position="114,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_5" position="135,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_6" position="156,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_7" position="177,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_8" position="198,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_9" position="219,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_10" position="240,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_11" position="261,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_12" position="282,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_13" position="303,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_14" position="324,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+			<widget name="top_15" position="345,30" zPosition="6" size="20,8" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png" />
+		</screen>""","""
 		<screen name="InternetRadioOledDisplay" position="0,0" size="132,64" id="1">
 			<widget name="text1" position="4,0" size="132,14" font="Regular;12" halign="center" valign="center"/>
 			<widget name="text2" position="4,14" size="132,49" font="Regular;10" halign="center" valign="center"/>
@@ -115,6 +139,7 @@ class InternetRadioOledDisplay(Screen, InternetRadioVisualization):
 			<widget name="top_14" position="114,5" zPosition="6" size="7,2" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/topvalue-lcd-fs8.png" />
 			<widget name="top_15" position="122,5" zPosition="6" size="7,2" transparent="1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/topvalue-lcd-fs8.png" />
 		</screen>""")
+		
 
 
 	def __init__(self, session, parent):
@@ -123,6 +148,22 @@ class InternetRadioOledDisplay(Screen, InternetRadioVisualization):
 		self["text1"] =  Label(_("Internet Radio"))
 		self["text2"] = Label("")
 		self.onLayoutFinish.append(self.startRun)
+		
+		
+		# helper for skinning
+#		skincontent = ""
+#		skincontent2 = ""
+#		count = 16
+#		x = 0
+#		posx = 30
+#		while True:
+#			skincontent += "<widget name=\"progress_%d\" zPosition=\"3\" position=\"%d,25\" size=\"20,200\" transparent=\"1\" orientation=\"orBottomToTop\" pixmap=\"/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green-fs8.png\" />\n" % (x,posx)
+#			skincontent2 += "<widget name=\"top_%d\" position=\"%d,25\" zPosition=\"6\" size=\"20,8\" transparent=\"1\" pixmap=\"/usr/lib/enigma2/python/Plugins/Extensions/InternetRadio/images/bar_green_top-fs8.png\" />\n" % (x,posx)
+#			posx += 21
+#			x += 1
+#			if x == count:
+#				break
+		
 
 	def startRun(self):
 		self.setProperties()
