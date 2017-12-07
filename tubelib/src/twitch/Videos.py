d431834a39d4aa99f3da0b632b335cb397647159
reichi <sreichholf@users.noreply.github.com>
2017-12-07 09:02:18 +0100

    tubelib: implement use of config.usage.max_stream_resolution.value for twitch, too

--- tubelib/src/twitch/Videos.py
+++ tubelib/src/twitch/Videos.py
@@ -1,5 +1,6 @@
 from twisted.internet import threads
 from youtube_dl.YoutubeDL import YoutubeDL
+from Components.config import config
 from Tools.Log import Log
 
 class TwitchVideoUrlResolver(object):
@@ -10,6 +11,8 @@ class TwitchVideoUrlResolver(object):
 
 	KEY_FORMAT_ID = u"format_id"
 	KEY_URL = u"url"
+	KEY_HEIGHT = u"height"
+	KEY_FORMATS = u"formats"
 
 	def __init__(self, baseurl, callbacks=[], async=True):
 		self._canceled = False
@@ -22,12 +25,31 @@ class TwitchVideoUrlResolver(object):
 			self._request()
 
 
+	def _selectFormat(self, twitchInfo):
+		try:
+			maxres = int(config.usage.max_stream_resolution.value)
+		except:
+			maxres = 1080
+		url = ""
+		fmt = ""
+		current = maxres;
+		for twitchFmt in twitchInfo.get(self.KEY_FORMATS, []):
+			height = twitchFmt.get(self.KEY_HEIGHT, 0)
+			if height and height <= maxres and height > current:
+				url = str(twitchFmt[self.KEY_URL])
+				fmt = str(twitchFmt[self.KEY_FORMAT_ID])
+				current = height
+		if not url:
+			url = str(twitchInfo[self.KEY_URL])
+			fmt = str(twitchInfo[self.KEY_FORMAT_ID])
+		Log.w("Selected resolution: %s" %(fmt,))
+		return url, fmt
+
 	def _request(self):
 		ie_key = "TwitchStream"
 		try:
 			result = self._ytdl.extract_info(self._baseurl, ie_key=ie_key, download=False, process=True)
-			url = str(result[self.KEY_URL])
-			fmt = str(result[self.KEY_FORMAT_ID])
+			url, fmt = self._selectFormat(result)
 			self._onResult(True, url, fmt)
 		except Exception as e:
 			Log.w(e)
