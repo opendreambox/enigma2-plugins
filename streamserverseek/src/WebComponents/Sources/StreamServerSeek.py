34b050b3ce696e2ec6e0d702486d090523d066b1
Martin Hirte <martin@hirte.info>
2018-04-16 10:43:37 +0200

    Initial commit of streamserverseek

---
+++ streamserverseek/src/WebComponents/Sources/StreamServerSeek.py
@@ -0,0 +1,199 @@
+from Components.Sources.Source import Source
+
+import os.path
+
+if os.path.isfile("/usr/lib/enigma2/python/Plugins/SystemPlugins/GstRtspServer/StreamServerControl.py"):
+	from Plugins.SystemPlugins.GstRtspServer.StreamServerControl import streamServerControl
+else:
+	from Components.StreamServerControl import streamServerControl
+
+class StreamServerSeek(Source):
+	SEEK_TO = 0
+	SEEK_RELATIVE = 1
+	SEEK_CHAPTER = 2
+	GET_LENGTH = 3
+	GET_PLAY_POSITION = 4
+	PAUSE = 5
+	UNPAUSE = 6
+	FAST_FORWARD = 7
+	FAST_BACKWARD = 8
+	SLOW_MOTION = 9
+	PLAY = 10
+
+	REQUIRE_PAUSABLE = {PAUSE, UNPAUSE, FAST_FORWARD, FAST_BACKWARD, SLOW_MOTION, PLAY}
+	REQUIRE_SEEKABLE = {SEEK_TO, SEEK_RELATIVE, SEEK_CHAPTER, GET_LENGTH, GET_PLAY_POSITION}
+
+	_func = None
+	_unitMultiplier = 1
+
+	_pause = None
+	_seek = None
+	
+	res = None
+
+	def __init__(self, session, func):
+		Source.__init__(self)
+		self.session = session
+		self.func = func
+		
+	def pause(self):
+		return self._pause.pause()
+	
+	def unpause(self):
+		return self._pause.unpause()
+	
+	def fastForward(self, speed):
+		if speed == 0:
+			return self.unpause()
+		return self._pause.setFastForward(speed)
+
+	def fastBackward(self, speed):
+		return self.fastForward(speed * -1)
+	
+	def slowMotion(self, speed):
+		if speed == 0:
+			return self.unpause()
+		return self._pause.setSlowMotion(speed)
+	
+	def seekTo(self, pos):
+		return self._seek.seekTo(pos * self._unitMultiplier)
+	
+	def seekRelative(self, pos):
+		if pos > 0:
+			return self._seek.seekRelative(1, pos * self._unitMultiplier)
+		elif pos < 0:
+			return self._seek.seekRelative(-1, pos * self._unitMultiplier * -1)
+		return -1
+
+	def seekChapter(self, number):
+		return self._seek.seekChapter(number)
+	
+	def getLength(self):
+		result = self._seek.getLength()
+		if not result[0]:
+			if result[1] == 0:
+				return 0
+			return result[1] / self._unitMultiplier
+		return -1
+
+	def getPlayPosition(self):
+		result = self._seek.getPlayPosition()
+		if not result[0]:
+			if result[1] == 0:
+				return 0
+			return result[1] / self._unitMultiplier
+		return -1
+
+	def initVars(self):
+		if not streamServerControl._encoderService:
+			self.res = ( False, _("encoder service not running") )
+			return False
+
+		if self.func in self.REQUIRE_PAUSABLE:
+			pause = streamServerControl._encoderService.pause()
+			if not pause:
+				self.res = ( False, _("encoder service not pausable") )
+				return False
+			self._pause = pause
+		
+		if self.func in self.REQUIRE_SEEKABLE:
+			seek = streamServerControl._encoderService.seek()
+			if not seek:
+				self.res = ( False, _("encoder service not seekable") )
+				return False
+			self._seek = seek
+		
+		return True
+
+	def handleCommand(self, params):
+		if not self.initVars():
+			return
+		
+		pos = None
+		number = None
+		speed = None
+
+		if self.func is self.SEEK_TO or self.func is self.SEEK_RELATIVE:
+			pos = params.get("pos", None)
+			if not pos:
+				self.res = ( False, _("required parameter '%s' missing") % "pos" )
+				return
+			try:
+				pos = int(pos)
+			except Exception:
+				self.res = ( False, _("invalid parameter '%s'") % "pos" )
+				return
+
+		if self.func is self.SEEK_CHAPTER:
+			number = params
+			if not number:
+				self.res = ( False, _("required parameter '%s' missing") % "number" )
+				return
+			try:
+				number = int(number)
+			except Exception:
+				self.res = ( False, _("invalid parameter '%s'") % "number" )
+				return
+		
+		if self.func is self.FAST_FORWARD or self.func is self.FAST_BACKWARD or self.func is self.SLOW_MOTION:
+			speed = params
+			if not speed:
+				self.res = ( False, _("required parameter '%s' missing") % "speed" )
+				return
+			try:
+				speed = int(speed)
+			except Exception:
+				self.res = ( False, _("invalid parameter '%s'") % "speed" )
+				return
+		
+		if self.func is self.SEEK_TO or self.func is self.SEEK_RELATIVE or self.func is self.GET_LENGTH or self.func is self.GET_PLAY_POSITION:
+			self._unitMultiplier = 1
+			if self.func is self.GET_LENGTH or self.func is self.GET_PLAY_POSITION:
+				unit = params
+			else:
+				unit = params.get("unit", None)
+			if unit and unit == "sec":
+				self._unitMultiplier = 90000
+			elif unit and unit == "min":
+				self._unitMultiplier = 90000 * 60
+			elif unit:
+				self.res = ( False, _("invalid parameter '%s'") % "unit" )
+				return
+
+		if self.func is self.SEEK_TO:
+			result = self.seekTo(pos)
+		elif self.func is self.SEEK_RELATIVE:
+			result = self.seekRelative(pos)
+		elif self.func is self.SEEK_CHAPTER:
+			result = self.seekChapter(number)
+		elif self.func is self.GET_LENGTH:
+			result = self.getLength()
+		elif self.func is self.GET_PLAY_POSITION:
+			result = self.getPlayPosition()
+		elif self.func is self.FAST_FORWARD:
+			result = self.fastForward(speed)
+		elif self.func is self.FAST_BACKWARD:
+			result = self.fastBackward(speed)
+		elif self.func is self.SLOW_MOTION:
+			result = self.slowMotion(speed)
+
+		self.res = ( True, result )
+	
+	def getResult(self):
+		if not self.res:
+			if not self.initVars():
+				return self.res
+			elif self.func is self.GET_LENGTH:
+				return ( True, self.getLength() )
+			elif self.func is self.GET_PLAY_POSITION:
+				return ( True, self.getPlayPosition() )
+			elif self.func is self.PAUSE:
+				return ( True, self.pause() )
+			elif self.func is self.UNPAUSE or self.func is self.PLAY:
+				return ( True, self.unpause() )
+			else:
+				return ( False, _("required parameter(s) missing") )
+
+		return self.res
+
+	result = property(getResult)
---
+++ streamserverseek/src/plugin.py
@@ -0,0 +1,81 @@
+from Plugins.Plugin import PluginDescriptor
+
+from enigma import eStreamServer, eEnv
+from Components.StreamServerControl import streamServerControl
+
+import os.path
+import urllib
+
+from Tools.Log import Log
+from urlparse import parse_qs
+
+from Plugins.Extensions.WebInterface.WebChilds.Toplevel import addExternalChild
+from Plugins.Extensions.WebInterface.WebChilds.Screenpage import ScreenPage
+
+from twisted.web import static
+from twisted.python import util
+
+def StreamServerControl__onUriParametersChanged(parameters):
+	self = streamServerControl
+
+	Log.i("%s" %(parameters))
+	params = parse_qs(parameters)
+
+	print "[StreamserverSeek] Old params: %s" % params
+	self._seekToMin = 0
+	if int(self.config.streamserver.source.value) == eStreamServer.INPUT_MODE_BACKGROUND:
+		ts = False
+		if "ts" in params:
+			ts = params.get("ts", [False])[0]
+			del params["ts"]
+		self._seekToMin = 0
+		min = 0
+		if "min" in params:
+			tmp = params.get("min", [False])[0]
+			if "min" in params:
+				del params["min"]
+			try:
+				min = int(tmp)
+			except Exception, e:
+				pass
+		if ts and os.path.isfile(ts):
+			params["ref"] = ["1:0:0:0:0:0:0:0:0:0:" + urllib.quote(ts)]
+			if min and min > 0:
+				self._seekToMin = min
+				print "[StreamserverSeek] Will seek to %d minutes" % self._seekToMin
+		print "[StreamserverSeek] New params: %s" % params
+
+	self._applyUriParameters(params)
+	for fnc in self.onUriParametersChanged:
+		fnc(params)
+
+streamServerControl._onUriParametersChanged_conn = streamServerControl._streamServer.uriParametersChanged.connect(StreamServerControl__onUriParametersChanged)
+
+Orig_StreamServerControl__startEncoderService = streamServerControl._startEncoderService
+
+def StreamServerControl__startEncoderService(service):
+	self = streamServerControl
+	result = Orig_StreamServerControl__startEncoderService(service)
+
+	if result == self.ENCODER_SERVICE_SET or result == self.ENCODER_SERVICE_ALREADY_ACTIVE:
+		if self._seekToMin and self.sourceState == True and self._encoderService and int(self.config.streamserver.source.value) == eStreamServer.INPUT_MODE_BACKGROUND:
+			seek = self._encoderService.seek()
+			if seek:
+				print "[StreamserverSeek] Seeking to %d minutes" % self._seekToMin
+				seek.seekTo(self._seekToMin * 60 * 90000)
+			self._seekToMin = 0
+
+streamServerControl._startEncoderService = StreamServerControl__startEncoderService
+
+File = static.File
+
+def autostart(reason,**kwargs):
+	if "session" in kwargs:
+		print "[StreamserverSeek] init"
+		session = kwargs["session"]
+		root = File(eEnv.resolve("${libdir}/enigma2/python/Plugins/Extensions/StreamServerSeek/web-data"))
+		root.putChild("web", ScreenPage(session, util.sibpath(__file__, "web"), True) )
+		addExternalChild( ("streamserverseek", root) )
+
+def Plugins(**kwargs): 
+	return [PluginDescriptor(where = [PluginDescriptor.WHERE_SESSIONSTART], fnc = autostart)]
