cef3d8af0e7fc0b57e4eb51aa367c39be9e6a2ef
Andreas Oberritter <obi@opendreambox.org>
2019-05-19 13:57:30 +0200

    dreamirc: python-modernize: lib2to3.fixes.fix_numliterals
    Change-Id: Ia632f1eec766cc30a3b0bc82a0cdfed9dc3c3457

--- dreamirc/src/protocols/irc.py
+++ dreamirc/src/protocols/irc.py
@@ -53,10 +53,10 @@ import socket
 from os import path
 
 NUL = chr(0)
-CR = chr(015)
-NL = chr(012)
+CR = chr(0o15)
+NL = chr(0o12)
 LF = NL
-SPC = chr(040)
+SPC = chr(0o40)
 
 CHANNEL_PREFIXES = '&#!+'
 
@@ -1905,7 +1905,7 @@ class DccFileReceive(DccFileReceiveBasic):
 
 # CTCP constants and helper functions
 
-X_DELIM = chr(001)
+X_DELIM = chr(0o01)
 
 def ctcpExtract(message):
     """Extract CTCP data from a string.
@@ -1950,7 +1950,7 @@ def ctcpExtract(message):
 
 # CTCP escaping
 
-M_QUOTE= chr(020)
+M_QUOTE= chr(0o20)
 
 mQuoteTable = {
     NUL: M_QUOTE + '0',
