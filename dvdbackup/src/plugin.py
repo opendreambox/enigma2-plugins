d931320489dcc05cbf5f2478a4a9fe0f2b8ac27c
Andreas Oberritter <obi@opendreambox.org>
2019-05-19 13:57:53 +0200

    dvdbackup: python-modernize: lib2to3.fixes.fix_except
    Change-Id: Ibb0c307bf190a62c5f16992e92e3f41736516e32

--- dvdbackup/src/plugin.py
+++ dvdbackup/src/plugin.py
@@ -49,7 +49,7 @@ def eject(dev):
 		ioctl_flag = int(0x5309)
 		ioctl(cd.fileno(), ioctl_flag)
 		cd.close()
-	except IOError, err:
+	except IOError as err:
 		print err
 
 #################################################
