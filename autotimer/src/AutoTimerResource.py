4f0829d9c42e7612055464ff77d422142f584ba0
Andreas Oberritter <obi@opendreambox.org>
2019-05-19 13:56:25 +0200

    autotimer: python-modernize: libmodernize.fixes.fix_urllib_six
    Change-Id: Ic800f2cf5589797297798de3c2eb845dedc70e33

--- autotimer/src/AutoTimerResource.py
+++ autotimer/src/AutoTimerResource.py
@@ -6,7 +6,7 @@ from twisted.internet import reactor
 from twisted.web import http, resource, server
 import threading
 try:
-	from urllib import unquote
+	from six.moves.urllib.parse import unquote
 except ImportError as ie:
 	from urllib.parse import unquote
 from ServiceReference import ServiceReference
