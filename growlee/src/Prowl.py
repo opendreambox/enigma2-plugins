8a1eb9f8f93d8e8298828e04377083c2aeef0b15
Andreas Oberritter <obi@opendreambox.org>
2019-05-19 14:03:42 +0200

    growlee: python-modernize: libmodernize.fixes.fix_urllib_six
    Change-Id: I91f17ef204ac1251ee6875609cad66ae950567f0

--- growlee/src/Prowl.py
+++ growlee/src/Prowl.py
@@ -2,7 +2,7 @@ from twisted.web.client import getPage
 from twisted.internet.defer import Deferred
 from twisted.internet import reactor
 try:
-	from urllib import urlencode
+	from six.moves.urllib.parse import urlencode
 except ImportError as ie:
 	from urllib.parse import urlencode
 
