db86e0417944a09de20f80f89066d563e7987279
Andreas Oberritter <obi@opendreambox.org>
2019-05-19 13:54:50 +0200

    audiosync: python-modernize: libmodernize.fixes.fix_dict_six
    Change-Id: Ib14df5d763d8df969165c773b82889c7dc7dfda7

--- audiosync/src/AC3main.py
+++ audiosync/src/AC3main.py
@@ -11,6 +11,7 @@ from Screens.ChoiceBox import ChoiceBox
 from Screens.HelpMenu import HelpableScreen
 from Screens.MessageBox import MessageBox
 from Screens.Screen import Screen
+import six
 
 class AC3LipSync(Screen, HelpableScreen, MovableScreen):
 
@@ -241,7 +242,7 @@ class AC3SetCustomValue:
 
     def getKeyList(self):
         keyList = []
-        for i,iValue in self.keyStep.iteritems():
+        for i,iValue in six.iteritems(self.keyStep):
             if i != "0":
                 keyList.append((_("Key %(key)s (current value: %(value)i ms)") %dict(key=i, value=iValue),i))
         return keyList
